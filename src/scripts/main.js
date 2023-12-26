aa58a3c67e51fed3ff5cfcd0ecbb40ca7d7df2c5
Fabio Henrique <Farabio973@hotmail.com>
2023-12-25 23:32:34 -0300

    Changes to the About/Js Scripts and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,15 @@ const state = {
        boxMenuOptions: document.querySelectorAll('ul')[0],
        menuOptions: document.querySelectorAll('li'),
        imgMenu: document.querySelectorAll('img')[1],
+       aboutButton: document.querySelectorAll('li a')[0],
+       technologiesButton: document.querySelectorAll('li a')[1],
+       projectsButton: document.querySelectorAll('li a')[2],
+    },
+    sections: {
+        home: document.querySelector('#home'),
+        about: document.querySelector('#about'),
+        technologies: document.querySelector('#technologies'),
+        projects: document.querySelector('#projects'),
     },
     projects: {
         project1: {
@@ -21,13 +30,38 @@ const state = {
     },
 }
 
-// ------- Menu -----------------------------------------------------
 state.menu.menuIcon.addEventListener('click', showMenuBox);
 
 for (let i = 0; i <= 2; i++){
     state.menu.menuOptions[i].addEventListener('click', hiddenMenuOption);   
 }
 
+state.sections.home.addEventListener('mouseover', ()=>{
+    state.menu.aboutButton.classList.remove("selected");
+    state.menu.projectsButton.classList.remove("selected");
+    state.menu.technologiesButton.classList.remove("selected");
+})
+
+state.sections.about.addEventListener('mouseover', ()=>{
+    state.menu.aboutButton.classList.add("selected");
+
+    state.menu.projectsButton.classList.remove("selected");
+    state.menu.technologiesButton.classList.remove("selected");
+})
+
+state.sections.technologies.addEventListener('mouseover', ()=>{
+    state.menu.technologiesButton.classList.add("selected");
+
+    state.menu.projectsButton.classList.remove("selected");
+    state.menu.aboutButton.classList.remove("selected");
+})
+
+state.sections.projects.addEventListener('mouseover', ()=>{
+    state.menu.projectsButton.classList.add("selected");
+
+    state.menu.aboutButton.classList.remove("selected");
+    state.menu.technologiesButton.classList.remove("selected");
+})
 
 // -------- Functions Menu ---------
 function showMenuBox() {
@@ -42,7 +76,12 @@ function showMenuBox() {
     }
 }
 
+function sectionSelected() {
+    
+}
+
 function hiddenMenuOption() {
     state.menu.boxMenuOptions.classList.add("hidden");
     state.menu.imgMenu.src = "./src/assets/icons/botao-de-menu-de-tres-linhas-horizontais-branco.png";
 }
+
