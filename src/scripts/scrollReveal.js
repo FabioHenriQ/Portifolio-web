e9970d8dbf2d4373057718c1a0c91b9cff0ba876
Fabio Henrique <Farabio973@hotmail.com>
2024-01-09 13:47:21 -0300

    Changed some styles and animations

--- src/scripts/scrollReveal.js
+++ src/scripts/scrollReveal.js
@@ -1,19 +1,23 @@
-const sections = ['#home', '#about'];
+const sections = ['#home'];
 const soloElementsSkills = ['.card'];
 const soloElementsProjects = ['.project-card'];
+const soloElementsAbout = ['.learning-about li'];
 
 
-WhatScrollReveal(sections, 2000, false);
-WhatScrollReveal(soloElementsSkills, 2000);
-WhatScrollReveal(soloElementsProjects, 2000);
+WhatScrollReveal(sections, 2000, 'top', false);
+WhatScrollReveal(soloElementsAbout, 2000, 'left', false);
+WhatScrollReveal(soloElementsSkills, 1500);
+WhatScrollReveal(soloElementsProjects, 1500);
 
 
-function WhatScrollReveal(array, duration, again = true) {
+function WhatScrollReveal(array, duration, origin = 'top', again = true) {
     window.revelar = ScrollReveal({reset:again});
 
     for (let i = 0; i <= array.length; i++) {
         ScrollReveal().reveal(array[i], {
+            origin: origin,
             duration: duration,
+            distance: '60px'
         });
     }
 }
