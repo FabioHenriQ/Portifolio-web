ca6573a97557e2e74c366ec8f8a0ece7b90cc6af
Fabio Henrique <Farabio973@hotmail.com>
2024-02-07 12:32:14 -0300

    Added indicator in the menu

--- src/scripts/buttonUp.js
+++ src/scripts/buttonUp.js
@@ -1,5 +1,12 @@
 window.addEventListener('scroll', () => {
     let buttonUp = document.querySelectorAll('.button-up')[0];
-
     buttonUp.classList.toggle('show-button-up', window.scrollY > 0);
+
+    let menuOptions = document.querySelectorAll(".options-menu-desktop a");
+
+        
+    menuOptions[0].classList.toggle('selected', window.scrollY < 500);
+    menuOptions[1].classList.toggle('selected', window.scrollY > 500 && window.scrollY < 1000);
+    menuOptions[2].classList.toggle('selected', window.scrollY > 1000 && window.scrollY < 1600);
+    menuOptions[3].classList.toggle('selected', window.scrollY > 1600);
 })
